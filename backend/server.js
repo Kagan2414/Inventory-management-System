e776dcff3a77b8b3e1e7d407cbf93280f138cba8
Kagan2414 <kaganeshwar414@gmail.com>
2025-11-23 22:08:14 +0530

    23/11/25

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+   origin: "*",
+   methods: "GET,POST,PUT,DELETE"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
